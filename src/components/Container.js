9ad263f76b77d8a701a738ca583be7b519b4741b
Abid05 <now.abidali@gmail.com>
2021-03-18 18:04:49 +0500

    multiple titles added

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,21 +1,31 @@
+import React from "react";
 import { useCallback, useState } from "react";
 import { useDrop } from "react-dnd";
 import { ItemsTypes } from "./ItemsTypes";
 import MemeImage from "./MemeImage";
 import update from "immutability-helper";
- 
-export const Container = ({img, text}) => {
-  const [boxes, setBoxes] = useState({
-    a: { top: 20, left: 80, title: text}, 
-    b: { top: 120, left: 180, title: text}, 
-  });
+export const Container = ({ img, text, moreTitle }) => {
+  const [boxes, setBoxes] = useState({});
+  React.useEffect(() => {
+    const obj = {};
+    moreTitle?.map((more, i) => {
+      obj[i] = {
+        ...obj[i],
+        top: 23,
+        left: 23,
+        title: more.title,
+      };
+    });
+    console.log(obj);
+    setBoxes(obj);
+  }, [moreTitle.length]);
   const moveBox = useCallback(
     (id, left, top) => {
       setBoxes(
         update(boxes, {
           [id]: {
-            $merge: { left, top }
-          }
+            $merge: { left, top },
+          },
         })
       );
     },
@@ -30,20 +40,28 @@ export const Container = ({img, text}) => {
         const top = Math.round(item.top + delta.y);
         moveBox(item.id, left, top);
         return undefined;
-      }
+      },
     }),
     [moveBox]
   );
   return (
-    <div ref={drop} style={{position: "relative", backgroundImage: `url(${img})`, backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center", height: "100%"}}>
+    <div
+      ref={drop}
+      style={{
+        position: "relative",
+        backgroundImage: `url(${img})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100%",
+      }}
+    >
       {Object.keys(boxes).map((key) => {
         const { left, top } = boxes[key];
         return (
-         
           <MemeImage key={key} id={key} left={left} top={top}>
-            <h2  className="meme_img_text">{text}</h2>
+            <h2 className="meme_img_text">{text}</h2>
           </MemeImage>
-           
         );
       })}
     </div>
